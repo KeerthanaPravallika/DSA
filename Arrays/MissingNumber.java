308aeb71358f68a56e4afa91142903f3ea283f57
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-05-29 23:38:34 +0530

    Create MissingNumber.java

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,17 @@
+/* 
+Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+*/
+
+class Solution {
+    int MissingNumber(int array[], int n) {
+        // Your Code Here
+        int sumt = (n*(n+1))/2;
+        int i,sumObtain=0;
+        for(i=0;i<n-1;i++)
+        {
+            sumObtain += array[i];
+        }
+        return sumt-sumObtain;
+    }
+}

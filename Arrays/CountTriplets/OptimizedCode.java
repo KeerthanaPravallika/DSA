b8ea29c90762be57d505aa0d2a5a59e21250667d
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-06-25 15:53:16 +0530

    Update OptimizedCode.java

--- Arrays/CountTriplets/OptimizedCode.java
+++ Arrays/CountTriplets/OptimizedCode.java
@@ -1,9 +1,20 @@
-
+/*
+Given an array of distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.
+Input:
+N = 4
+arr[] = {1, 5, 3, 2}
+Output: 2
+Explanation: There are 2 triplets: 
+1 + 2 = 3 and 3 +2 = 5 
+*/
+class Solution {
+    int countTriplet(int arr[], int n) {
+        // code here
 
  
-   Arrays.sort(arr);
-   int i,j,k,count=0 ;
-        
+        Arrays.sort(arr);
+        int i,j,k,count=0 ;
+
         for(i = n-1;i >=0 ;i--)
         {
             j = 0;

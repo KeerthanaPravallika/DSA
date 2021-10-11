613edd03f0250228cc4b2e4145611533abb0a96f
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-10-11 10:09:08 +0530

    Create Xpattern.py

---
+++ Patterns/Xpattern.py
@@ -0,0 +1,34 @@
+'''
+Write a program to take String if the length of String is odd print X pattern otherwise print INVALID.
+
+Input Format:
+Take a String as input from stdin.
+
+Output Format:
+print the desired Pattern or INVALID.
+
+Example Input:
+edyst
+
+Output:
+
+e   t
+ d s 
+  y  
+ d s 
+e   t
+'''
+word = input()
+
+if len(word) % 2 == 0:
+    print("INVALID")
+else:
+    n = len(word)
+    for i in range(n):
+        for j in range(n):
+            if(i == j) or (j == n-i-1):
+                print(word[j],end='')
+            else:
+                print(' ',end='')
+        print()
+

84860f95206dcbdc49ea0a8438d5e7f44e89a1d2
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2024-07-02 09:09:21 +0530

    Added files

--- Graphes/course_schedule_2.cpp
+++ Graphes/course_schedule_2.cpp
@@ -1,32 +1,35 @@
 /*
 
-Link : https://leetcode.com/problems/course-schedule/description/
+Link : https://leetcode.com/problems/course-schedule-ii/description/
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 
 For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
-Return true if you can finish all courses. Otherwise, return false.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
 
  
 
 Example 1:
 
 Input: numCourses = 2, prerequisites = [[1,0]]
-Output: true
-Explanation: There are a total of 2 courses to take. 
-To take course 1 you should have finished course 0. So it is possible.
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
 Example 2:
 
-Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
-Output: false
-Explanation: There are a total of 2 courses to take. 
-To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+Example 3:
+
+Input: numCourses = 1, prerequisites = []
+Output: [0]
 
 
 */
 
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> adj[numCourses];
         vector<int> result;
 		queue<int> q;
@@ -60,8 +63,7 @@ public:
 		    
 		}
 		if(result.size() == numCourses)
-		    return true;
-		return false;
-
+		    return result;
+		return {};
     }
 };

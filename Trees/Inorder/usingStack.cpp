415ffb3491f281df063d638d6932074d837152e4
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2024-12-19 15:21:31 +0530

    added file

---
+++ Trees/Inorder/usingStack.cpp
@@ -0,0 +1,62 @@
+/*
+
+Link : https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> res;
+        TreeNode* node = root;
+
+    /*
+    Inorder means left root right
+
+    So first it will check left side anf=d then when it is null, it will print and check towards right
+    */
+        while(true)
+        {
+            if(node != NULL)
+            {
+                st.push(node);
+                node = node->left;
+            }
+            else
+            {
+                if(st.empty()) break;
+
+                node = st.top();
+                res.push_back(node->val);
+                st.pop();
+                node = node->right;
+            }
+
+        }
+        return res;
+        
+    }
+};

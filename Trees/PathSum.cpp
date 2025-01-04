62e00ec562514337b22d46eba9b1e6a4b2a37e6b
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2025-01-04 10:30:12 +0530

    path sum

---
+++ Trees/PathSum.cpp
@@ -0,0 +1,38 @@
+/*
+
+Link : https://leetcode.com/problems/path-sum/description
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (!root) return false;
+        
+        targetSum -= root->val;
+        
+        if (!root->left && !root->right) {
+            return targetSum == 0;
+        }
+        
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    }
+};

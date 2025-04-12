bc82eb673ed077f5cbaecd3fcf276feaf25594f9
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2025-04-12 15:40:24 +0530

    Changed lca

--- Trees/LCA.cpp
+++ Trees/LCA.cpp
@@ -65,3 +65,29 @@ public:
             return root;
     }
 };
+
+
+// 2nd approach
+
+class Solution {
+    public:
+        TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+            if(root == NULL){
+                return NULL;
+            }
+    
+            if(root->val == p->val || root->val == q->val){
+                return root;
+            }
+    
+            TreeNode* lca_left = lowestCommonAncestor(root->left, p, q);
+            TreeNode* lca_right = lowestCommonAncestor(root->right, p, q);
+    
+            if(lca_left && lca_right){
+                return root;
+            }
+    
+            return lca_left ? lca_left : lca_right;
+            
+        }
+    };
